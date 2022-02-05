90eeb9b59236ddee5ba8e2b46e32d234058da8c9
Popov A.S <antoxamad@gmail.com>
2022-02-05 23:40:43 +0300

    Велосипед с Redux. Перенос данных в state.js

--- src/index.js
+++ src/index.js
@@ -3,33 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-let posts = [
-  { id: 1, message: "Hello, how are you?", likesCount: 15 },
-  { id: 2, message: "My ferst post!", likesCount: 50 },
-  { id: 3, message: "Hello my Coder", likesCount: 777 },
-];
-
-let dialogs = [
-  { id: 1, name: "Anton" },
-  { id: 2, name: "Miha" },
-  { id: 3, name: "Solomaha" },
-  { id: 4, name: "Kate" },
-  { id: 5, name: "Igor" },
-];
-
-let messages = [
-  { id: 1, message: "Hi" },
-  { id: 2, message: "Hi how i you PopovCode" },
-  { id: 3, message: "Yo" },
-  { id: 4, message: "Yo" },
-  { id: 5, message: "Yo" },
-  { id: 6, message: "PopovCode test message" },
-];
+import state from "./redux/state";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages} />
+    <App state={state} />
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/redux/state.js
@@ -0,0 +1,29 @@
+let state = {
+  profilePage: {
+    posts: [
+      { id: 1, message: "Hello, how are you?", likesCount: 15 },
+      { id: 2, message: "My ferst post!", likesCount: 50 },
+      { id: 3, message: "Hello my Coder", likesCount: 777 },
+    ],
+  },
+
+  dialogsPage: {
+    dialogs: [
+      { id: 1, name: "Anton" },
+      { id: 2, name: "Miha" },
+      { id: 3, name: "Solomaha" },
+      { id: 4, name: "Kate" },
+      { id: 5, name: "Igor" },
+    ],
+    messages: [
+      { id: 1, message: "Hi" },
+      { id: 2, message: "Hi how i you PopovCode" },
+      { id: 3, message: "Yo" },
+      { id: 4, message: "Yo" },
+      { id: 5, message: "Yo" },
+      { id: 6, message: "PopovCode test message" },
+    ],
+  },
+};
+
+export default state;
